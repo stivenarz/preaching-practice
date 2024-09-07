8911a2f915b2392e7367ed1e5798abaacf2b3ccb
Stiven Arz <120063449+stivenarz@users.noreply.github.com>
2024-09-07 10:06:30 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,6 @@
 export const environment = {
   production: false,
   
-  firebaseConfig: {
-    apiKey: "AIzaSyAsDwtg90bfwA08X9NPd4kYqSNZyx6EJm4",
-    authDomain: "preaching-practice.firebaseapp.com",
-    projectId: "preaching-practice",
-    storageBucket: "preaching-practice.appspot.com",
-    messagingSenderId: "1049372109402",
-    appId: "1:1049372109402:web:28317a2123d504af4f0663"
-  },
-  
 };
 
 /*
